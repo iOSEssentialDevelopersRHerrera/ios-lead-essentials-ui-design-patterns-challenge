3a8c69aa267e8e1b5957b95e156b41706deb9091
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2021-01-25 00:28:45 -0500

    Snapshot error tests

--- MVVM Module/Tests/Feed UI/FeedUISnapshotTests.swift
+++ MVVM Module/Tests/Feed UI/FeedUISnapshotTests.swift
@@ -15,15 +15,15 @@ class FeedUISnapshotTests: XCTestCase {
 	//
 	//  ***********************
 	
-//	func test_emptyFeed() {
-//		let sut = makeSUT()
-//
-//		sut.display(emptyFeed())
-//
-//		assert(snapshot: sut.snapshot(for: .iPhone8(style: .light)), named: "EMPTY_FEED_light")
-//		assert(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "EMPTY_FEED_dark")
-//	}
-//
+	func test_emptyFeed() {
+		let sut = makeSUT()
+
+		sut.display(emptyFeed())
+
+		assert(snapshot: sut.snapshot(for: .iPhone8(style: .light)), named: "EMPTY_FEED_light")
+		assert(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "EMPTY_FEED_dark")
+	}
+
 //	func test_feedWithError() {
 //		let sut = makeSUT()
 //
@@ -32,7 +32,7 @@ class FeedUISnapshotTests: XCTestCase {
 //		assert(snapshot: sut.snapshot(for: .iPhone8(style: .light)), named: "FEED_WITH_ERROR_light")
 //		assert(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "FEED_WITH_ERROR_dark")
 //	}
-	
+//	
 	// MARK: - Helpers
 	
 	private func makeSUT() -> FeedViewController {
@@ -66,7 +66,7 @@ private class FeedLoaderStub: FeedLoader {
 
 private extension FeedViewController {
 	func display(errorMessage: String) {
-		fatalError("Must be implemented - follow the MVC solution as a guide")
+		errorView?.show(message: errorMessage)
 	}
 	
 	func display(_ feed: [FeedImageCellController]) {
