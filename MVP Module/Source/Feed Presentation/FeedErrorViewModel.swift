409049901fa4d0b4ed937bc43912c2ac70d802e8
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2021-01-28 20:15:00 -0500

    Move FeedErrorViewModel to its own file

---
+++ MVP Module/Source/Feed Presentation/FeedErrorViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  FeedErrorViewModel.swift
+//  MVP
+//
+//  Created by Ricardo Herrera Petit on 1/28/21.
+//  Copyright © 2021 Essential Developer. All rights reserved.
+//
+
+import Foundation
+
+struct FeedErrorViewModel {
+	let errorMessage:String?
+}
